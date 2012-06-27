142a7d0b4f14d4156700e80b16be0bdaa806f346
Joseph Reeves <iknowjoseph@gmail.com>
2012-06-27 07:50:30 +0200

    Update master

--- db/migrate/20111108093853_create_projects.rb
+++ db/migrate/20111108093853_create_projects.rb
@@ -10,10 +10,10 @@ class CreateProjects < ActiveRecord::Migration
       add_column :users, :project_id, :integer, :default => 1
 
       # Project.create!(:name => "Generic", :partial => "generic")
-      Project.create!(:name => "BBB Home Owner", :partial => "bbb_home_owner")
-      Project.create!(:name => "BBB Door To Door", :partial => "bbb_door_door")
-      Project.create!(:name => "ACCESS Poverty Mapping", :partial => "access_poverty_mapping")
-      Project.create!(:name => "PNPM Poverty Mapping", :partial => "pnpm_poverty_mapping")
+      #Project.create!(:name => "BBB Home Owner", :partial => "bbb_home_owner")
+      #Project.create!(:name => "BBB Door To Door", :partial => "bbb_door_door")
+      #Project.create!(:name => "ACCESS Poverty Mapping", :partial => "access_poverty_mapping")
+      #Project.create!(:name => "PNPM Poverty Mapping", :partial => "pnpm_poverty_mapping")
 
    end
 end
