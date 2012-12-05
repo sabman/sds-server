3282080e896ba7de515f378fc9726df6edf6fc3a
Tim Waters <chippy2005@gmail.com>
2012-12-05 11:51:12 +0000

    ignore misc files, etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111114103406) do
+ActiveRecord::Schema.define(:version => 20120426190642) do
 
   create_table "changesets", :force => true do |t|
     t.integer  "user_id"
