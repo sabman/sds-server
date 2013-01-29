ae4a2f5c6f7d2fd081db5f30ce72bcdf4e281272
Tim Waters <chippy2005@gmail.com>
2013-01-29 16:26:00 +0000

    Add docs to explain how to force the available language choices. refs #15

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,6 +4,10 @@ I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 # set default locale to something other than :en
 I18n.default_locale = :en
 
+#uncomment to force restrict the list of available languages
+#by default, it will look in config/locales and populate this array from looking at whats there
+#I18n.available_locales = [:en]
+
 if Rails.env.development? || Rails.env.test?
 
   # raises exception when there is a wrong/no i18n key
