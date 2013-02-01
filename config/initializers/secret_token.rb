70500b74cd9ba282a3a64d5543f288d3f7d4b7c8
Tim Waters <chippy2005@gmail.com>
2013-02-01 15:40:11 +0000

    Remove secret token from repo and generate it dynamically on the server!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,14 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HotJosm::Application.config.secret_token = '8fff2187f09114f92c25b50760b9b128022cbd9de49e8f6c9d49db0d16c2de43f5075d0fdb410e3b77b9f2ad7dd99ecc054cc2c73d9d337aee8501f78556aeec'
+#HotJosm::Application.config.secret_token = '8fff2187f09114f92c25b50760b9b128022cbd9de49e8f6c9d49db0d16c2de43f5075d0fdb410e3b77b9f2ad7dd99ecc054cc2c73d9d337aee8501f78556aeec'
+
+secret_file = File.join(Rails.root, "sds_secret")
+if File.exist?(secret_file)
+  secret = File.read(secret_file)
+else
+  secret = ActiveSupport::SecureRandom.hex(64)
+  File.open(secret_file, 'w') { |f| f.write(secret) }
+end
+  
+HotJosm::Application.config.secret_token = secret
